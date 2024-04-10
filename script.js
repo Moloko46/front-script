8d32837f68d676be8be4db2bb129acdcb86e52fc
vladislavs <vladislavs.kazimirenko@gmail.com>
2024-04-10 18:21:05 +0300

    added fix

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ class _PaymentIQCashier {
   url = "https://payaggregator.com/main/waiting-page/";
 
   data = {};
-
+  events = {
+    success: () => {},
+  };
   api = {
+    on: (event) => {
+      for (let key in event) {
+        if (key === "success") {
+          this.events[key] = event[key];
+        }
+      }
+    },
     set: (data) => {
       this.data = { ...data.config };
     },
@@ -16,7 +25,11 @@ class _PaymentIQCashier {
     callbackfunc(this.api);
     this.data = { ...this.data, ...data };
     const url = this.createUrl(data);
+    console.log(this.data);
     this.createIframe(id, url, data);
+  }
+  emitSuccess(data) {
+    this.events.success(data);
   }
 
   createIframe(id, url, data) {
@@ -40,23 +53,23 @@ class _PaymentIQCashier {
   }
 }
 
-const data = {
-  merchantId: "87483265",
-  userId: "3415135",
-  sessionId: "222222",
-  environment: "dev",
-  method: "creditCard",
-  locale: "test",
-  providerType: "<?php echo $fields->providerType;?>",
-  mode: "<?php echo $fields->mode;?>",
-  attributes: "<?php echo json_encode($fields->attributesArr);?>",
-  user: "<?php echo json_encode($fields->userArr);?>",
-  autoProcess: "<?php echo ($fields->autoProcess ? 'true' : 'false');?>",
-  predefinedValues: false,
-  containerHeight: "100%",
-  containerMinHeight: "800px",
-  showFooter: false,
-  showHeader: false,
-  dev_close_transaction_with_status: "fail",
-  isWhiteList: true,
-};
+// const data = {
+//   merchantId: "87483265",
+//   userId: "3415135",
+//   sessionId: "222222",
+//   environment: "dev",
+//   method: "creditCard",
+//   locale: "test",
+//   providerType: "<?php echo $fields->providerType;?>",
+//   mode: "<?php echo $fields->mode;?>",
+//   attributes: "<?php echo json_encode($fields->attributesArr);?>",
+//   user: "<?php echo json_encode($fields->userArr);?>",
+//   autoProcess: "<?php echo ($fields->autoProcess ? 'true' : 'false');?>",
+//   predefinedValues: false,
+//   containerHeight: "100%",
+//   containerMinHeight: "800px",
+//   showFooter: false,
+//   showHeader: false,
+//   dev_close_transaction_with_status: "fail",
+//   isWhiteList: true,
+// };
