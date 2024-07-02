cddf4064288a803a77eb78f7bec51c3df249097c
vladislavs <vladislavs.kazimirenko@gmail.com>
2024-07-02 21:50:26 +0300

    added fixes to sccript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 class _PaymentIQCashier {
-  url = "https://payaggregator.com/main/waiting-page/";
-
+  url = "https://payaggregator.com/main/waiting-session/";
+  postUrl = "https://payaggregator.com/main/set-session-to-redis/";
   data = {};
   events = {
     success: () => {},
@@ -22,8 +22,13 @@ class _PaymentIQCashier {
   };
 
   constructor(id, data, callbackfunc) {
+    this.Init(id, data, callbackfunc);
+  }
+
+  async Init(id, data, callbackfunc) {
     callbackfunc(this.api);
     this.data = { ...this.data, ...data };
+    const setSession = await this.postRequestToCreateSession(this.data);
     const url = this.createUrl(this.data);
     console.log(this.data);
     this.createIframe(id, url);
@@ -44,35 +49,27 @@ class _PaymentIQCashier {
     htmlBlock.appendChild(iframe);
   }
 
+  async postRequestToCreateSession(data) {
+    return await fetch(this.postUrl, {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      //make sure to serialize your JSON body
+      body: JSON.stringify(data),
+    }).then((response) => {
+      console.log(response, "response from post request");
+      //do something awesome that makes the world a better place
+    });
+  }
+
   createUrl(data) {
     console.log(data, "data in creating url");
-    return `${this.url}${data.sessionId}/${data.userId}/${
-      data.merchantId
-    }?${new URLSearchParams({ ...data }).toString()}`;
+    return `${this.url}${data.sessionId}`;
   }
 
   responseWithCallBack(res) {
     return this.callbackfunc();
   }
 }
-
-// const data = {
-//   merchantId: "87483265",
-//   userId: "3415135",
-//   sessionId: "222222",
-//   environment: "dev",
-//   method: "creditCard",
-//   locale: "test",
-//   providerType: "<?php echo $fields->providerType;?>",
-//   mode: "<?php echo $fields->mode;?>",
-//   attributes: "<?php echo json_encode($fields->attributesArr);?>",
-//   user: "<?php echo json_encode($fields->userArr);?>",
-//   autoProcess: "<?php echo ($fields->autoProcess ? 'true' : 'false');?>",
-//   predefinedValues: false,
-//   containerHeight: "100%",
-//   containerMinHeight: "800px",
-//   showFooter: false,
-//   showHeader: false,
-//   dev_close_transaction_with_status: "fail",
-//   isWhiteList: true,
-// };
