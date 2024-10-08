c6f725b4e2e644933a2502458b9927c16bc1cd8e
Janis Kaveckis <janis@buysellvouchers.com>
2024-10-08 19:09:24 +0300

    hujovoje izmenenije

--- script.js
+++ script.js
@@ -1,73 +1,74 @@
 class _PaymentIQCashier {
-  url = "https://4re.team/main/waiting/";
-  postUrl = "https://4re.team/main/pending/";
-  data = {};
-  events = {
-    success: () => {},
-  };
-  api = {
-    on: (event) => {
-      for (let key in event) {
-        if (key === "success") {
-          this.events[key] = event[key];
-        }
-      }
-    },
-    set: (data) => {
-      this.data = { ...data.config };
-    },
-    css: (css) => {
-      return css;
-    },
-  };
+    url = "https://4re.team/main/waiting/";
+    postUrl = "https://4re.team/main/pending/";
+    data = {};
+    events = {
+        success: () => {},
+    };
+    api = {
+        on: (event) => {
+            for (let key in event) {
+                if (key === "success") {
+                    this.events[key] = event[key];
+                }
+            }
+        },
+        set: (data) => {
+            this.data = { ...data.config };
+        },
+        css: (css) => {
+            return css;
+        },
+    };
 
-  constructor(id, data, callbackfunc) {
-    this.Init(id, data, callbackfunc);
-  }
-
-  async Init(id, data, callbackfunc) {
-    callbackfunc(this.api);
-    this.data = { ...this.data, ...data };
-    console.log(this.data);
-    const setSession = await this.postRequestToCreateSession(this.data);
-    const url = this.createUrl(this.data);
-    if (this.data.redirect) {
-      window.open(url, "_blank");
-    } else {
-      this.createIframe(id, url);
+    constructor(id, data, callbackfunc) {
+        this.Init(id, data, callbackfunc);
     }
-  }
-  emitSuccess(data) {
-    this.events.success(data);
-  }
 
-  createIframe(id, url) {
-    const htmlBlock = document.querySelector(id);
-    const iframe = document.createElement("iframe");
-    iframe.setAttribute("src", url);
-    iframe.style.width = "100%";
-    iframe.style.height = "45rem";
-    htmlBlock.style.width = "100%";
-    htmlBlock.style.height = "45rem";
-    htmlBlock.appendChild(iframe);
-  }
+    async Init(id, data, callbackfunc) {
+        callbackfunc(this.api);
+        this.data = { ...this.data, ...data };
+        console.log(this.data);
+        const setSession = await this.postRequestToCreateSession(this.data);
+        const url = this.createUrl(this.data);
+        if (this.data.redirect) {
+            window.open(url, "_blank");
+        } else {
+            this.createIframe(id, url);
+        }
+    }
+    emitSuccess(data) {
+        this.events.success(data);
+    }
 
-  async postRequestToCreateSession(data) {
-    return await fetch(this.postUrl, {
-      method: "post",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    }).then((response) => {});
-  }
+    createIframe(id, url) {
+        const htmlBlock = document.querySelector(id);
+        const iframe = document.createElement("iframe");
+        iframe.setAttribute("src", url);
+        iframe.style.width = "100%";
+        iframe.style.height = "45rem";
+        htmlBlock.style.width = "100%";
+        htmlBlock.style.height = "45rem";
+        htmlBlock.appendChild(iframe);
+    }
 
-  createUrl(data) {
-    return `${this.url}${data.sessionId}`;
-  }
+    async postRequestToCreateSession(data) {
+        console.log(data);
+        return await fetch(this.postUrl, {
+            method: "post",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }).then((response) => {});
+    }
 
-  responseWithCallBack(res) {
-    return this.callbackfunc();
-  }
+    createUrl(data) {
+        return `${this.url}${data.sessionId}`;
+    }
+
+    responseWithCallBack(res) {
+        return this.callbackfunc();
+    }
 }
